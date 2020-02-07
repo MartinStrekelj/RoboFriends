8b37fc9964c152953882e10136915d218ed0b480
Martin Strekelj <martin.strekelj123@gmail.com>
2020-02-07 10:00:44 +0100

    Updates

---
+++ precache-manifest.db715e6e6995b296b36f25dc9e95faab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba62582b19509e437cd0f59e8a3e822",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "ffcc0c2d02d1a8a13987",
+    "url": "/RoboFriends/static/css/main.793c529e.chunk.css"
+  },
+  {
+    "revision": "4bd3b6016c456db939d5",
+    "url": "/RoboFriends/static/js/2.57b720db.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RoboFriends/static/js/2.57b720db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcc0c2d02d1a8a13987",
+    "url": "/RoboFriends/static/js/main.b6ba7357.chunk.js"
+  },
+  {
+    "revision": "3b5acba4a49444d74d44",
+    "url": "/RoboFriends/static/js/runtime-main.f1304c14.js"
+  }
+]);
